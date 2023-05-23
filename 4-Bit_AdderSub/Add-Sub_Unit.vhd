ad03424b6120cc75394befe25a2c6eb6cb667656
psychlone77 <127029023+psychlone77@users.noreply.github.com>
2023-05-24 00:07:56 +0530

    Update Add-Sub_Unit.vhd

--- 4-Bit_AdderSub/Add-Sub_Unit.vhd
+++ 4-Bit_AdderSub/Add-Sub_Unit.vhd
@@ -3,12 +3,13 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 
 entity Add_Sub_Unit is
-Port (  A : in STD_LOGIC_vector (3 downto 0);
-        B : in STD_LOGIC_vector (3 downto 0);
-        M : in STD_LOGIC;
-        S : out STD_LOGIC_vector(3 downto 0);
-        C_out : out STD_LOGIC;
-        V : out std_logic);
+Port (  A : in STD_LOGIC_vector (3 downto 0); --First Input
+        B : in STD_LOGIC_vector (3 downto 0); --Second Input, this input will be inversed when M=1
+        M : in STD_LOGIC; -- Add or Subtract Select
+        S : out STD_LOGIC_vector(3 downto 0); -- Output
+        C_out : out STD_LOGIC; -- Carry Out
+        Zero_Flag : out STD_LOGIC;
+        V : out STD_LOGIC); -- OverFlow bit
 end Add_Sub_Unit;
 
 
@@ -23,45 +24,49 @@ port (
         S: out std_logic;
         C_out: out std_logic);
 end component;
-SIGNAL FA0_S, FA0_C, FA1_S, FA1_C, FA2_S, FA2_C, FA3_S, FA3_C,varV : std_logic;
-signal varB :std_logic_vector(3 downto 0);
-begin
-        varB(0)<=B(0) xor M;
-        varB(1)<=B(1) xor M;
-        varB(2)<=B(2) xor M;
-        varB(3)<=B(3) xor M;
 
-        FA_0 : FA port map (
-                A => A(0),
-                B => varB(0),
-                C_in => M, -- Set to ground
-                S => S(0),
-                C_Out => FA0_C);
-                
-        FA_1 : FA port map (
-                A => A(1),
-                B => varB(1),
-                C_in => FA0_C,
-                S => S(1),
-                C_Out => FA1_C);
-                
-        FA_2 : FA port map (
-                A => A(2),
-                B => varB(2),
-                C_in => FA1_C,
-                S => S(2),
-                C_Out => FA2_C);
-                
-        FA_3 : FA port map (
-                A => A(3),
-                B => varB(3),
-                C_in => FA2_C,
-                S => S(3),
-                C_out => FA3_C); 
-                
-        --this part had to be changed from the Assignment code
-        varV <= FA2_C OR FA3_C; -- this part was added so the proper C_out was ouput
-        C_out <= FA3_C AND varV;
-        V <= varV;---- if V is 0 should not consider the carryout ----if v==1 shouold consider the carryout
+signal FA0_S, FA0_C, FA1_S, FA1_C, FA2_S, FA2_C, FA3_S, FA3_C, varV : std_logic;
+signal varB, varS : std_logic_vector(3 downto 0);
+
+begin
+    varB(0) <= B(0) XOR M;
+    varB(1) <= B(1) XOR M;
+    varB(2) <= B(2) XOR M;
+    varB(3) <= B(3) XOR M;
+
+    FA_0 : FA port map (
+            A => A(0),
+            B => varB(0),
+            C_in => M,
+            S => varS(0),
+            C_Out => FA0_C);
+            
+    FA_1 : FA port map (
+            A => A(1),
+            B => varB(1),
+            C_in => FA0_C,
+            S => varS(1),
+            C_Out => FA1_C);
+            
+    FA_2 : FA port map (
+            A => A(2),
+            B => varB(2),
+            C_in => FA1_C,
+            S => varS(2),
+            C_Out => FA2_C);
+            
+    FA_3 : FA port map (
+            A => A(3),
+            B => varB(3),
+            C_in => FA2_C,
+            S => varS(3),
+            C_out => FA3_C); 
+            
+            
+    varV <= FA2_C XOR FA3_C;
+    V <= varV; 
+    C_out <= FA3_C;
+    S <= varS;
+    Zero_Flag <= NOT(varS(0) OR varS(1) OR varS(2) OR varS(3) OR varV);
 
 end Behavioral;
