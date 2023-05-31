3609d7f30277b15d789eca07bc98c27124075c27
psychlone77 <127029023+psychlone77@users.noreply.github.com>
2023-05-31 09:33:36 +0530

    Update Reg_Bank.vhd

--- RegisterBank/Reg_Bank.vhd
+++ RegisterBank/Reg_Bank.vhd
@@ -23,8 +23,8 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity Reg_Bank is
-    Port ( D : in STD_LOGIC_VECTOR (3 downto 0);
-           RegEn : in STD_LOGIC_VECTOR (2 downto 0);
+    Port ( D : in STD_LOGIC_VECTOR (3 downto 0); --input bits
+           RegEn : in STD_LOGIC_VECTOR (2 downto 0); --which register to enable
            Clk : in STD_LOGIC;
            Out0 : out STD_LOGIC_VECTOR (3 downto 0);
            Out1 : out STD_LOGIC_VECTOR (3 downto 0);
@@ -52,20 +52,20 @@ component Decoder_3_to_8 is
             Y : out STD_LOGIC_VECTOR (7 downto 0));
 end component;
 
-signal varY : std_logic_vector (7 downto 0);
+signal varY : std_logic_vector (7 downto 0); --used to store Decoder output
+
 
 begin
 
-Decoder : Decoder_3_to_8 Port Map ( I => RegEn, En => '1', Y => varY);
+    Decoder : Decoder_3_to_8 Port Map ( I => RegEn, En => '1', Y => varY);
 
-Reg0 : Reg Port Map( D => "0000", En => '1', Clk => Clk, Q => Out0);
-Reg1 : Reg Port Map( D => D, En => varY(1), Clk => Clk, Q => Out1);
-Reg2 : Reg Port Map( D => D, En => varY(2), Clk => Clk, Q => Out2);
-Reg3 : Reg Port Map( D => D, En => varY(3), Clk => Clk, Q => Out3);
-Reg4 : Reg Port Map( D => D, En => varY(4), Clk => Clk, Q => Out4);
-Reg5 : Reg Port Map( D => D, En => varY(5), Clk => Clk, Q => Out5);
-Reg6 : Reg Port Map( D => D, En => varY(6), Clk => Clk, Q => Out6);
-Reg7 : Reg Port Map( D => D, En => varY(7), Clk => Clk, Q => Out7);
-
+    Reg0 : Reg Port Map( D => "0000", En => '1', Clk => Clk, Q => Out0); --Reg0 should always be "0000"
+    Reg1 : Reg Port Map( D => D, En => varY(1), Clk => Clk, Q => Out1);
+    Reg2 : Reg Port Map( D => D, En => varY(2), Clk => Clk, Q => Out2);
+    Reg3 : Reg Port Map( D => D, En => varY(3), Clk => Clk, Q => Out3);
+    Reg4 : Reg Port Map( D => D, En => varY(4), Clk => Clk, Q => Out4);
+    Reg5 : Reg Port Map( D => D, En => varY(5), Clk => Clk, Q => Out5);
+    Reg6 : Reg Port Map( D => D, En => varY(6), Clk => Clk, Q => Out6);
+    Reg7 : Reg Port Map( D => D, En => varY(7), Clk => Clk, Q => Out7);
 
 end Behavioral;
