ed9e40d3b02ccc7b13d9d31d6063f6737f50f8ae
psychlone77 <127029023+psychlone77@users.noreply.github.com>
2023-05-31 08:59:28 +0530

    Update TwoLine3bitMUX.vhd

--- 2_way_3-bit_MUX/TwoLine3bitMUX.vhd
+++ 2_way_3-bit_MUX/TwoLine3bitMUX.vhd
@@ -1,24 +1,3 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 05/21/2023 08:50:50 PM
--- Design Name: 
--- Module Name: 2way3bitMUX - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
@@ -33,8 +12,8 @@ architecture Behavioral of TwoLine3bitMUX is
 
 begin
 
-LineOut(0) <= (Line0(0) AND NOT(Sel)) OR (Line1(0) AND Sel);
-LineOut(1) <= (Line0(1) AND NOT(Sel)) OR (Line1(1) AND Sel);
-LineOut(2) <= (Line0(2) AND NOT(Sel)) OR (Line1(2) AND Sel);
+    LineOut(0) <= (Line0(0) AND NOT(Sel)) OR (Line1(0) AND Sel);
+    LineOut(1) <= (Line0(1) AND NOT(Sel)) OR (Line1(1) AND Sel);
+    LineOut(2) <= (Line0(2) AND NOT(Sel)) OR (Line1(2) AND Sel);
 
 end Behavioral;
